e77a8610a403b72170e868947162a3e724ab8b2a
Nos <zvezdazanatih@gmail.com>
2025-05-25 08:48:25 +0700

    11

--- app.js
+++ app.js
@@ -88,18 +88,6 @@ async function run() {
 
 
 
-//         app.get('/api', async (req, res) => {
-//       try {
-//         // Получаем все документы из коллекции
-//         const us = await collection.find().toArray();
-//         res.status(200).json(us);
-        
-
-//       } catch (err) {
-//         res.status(500).json({ message: "Ошибка при получении данных", error: err });
-//       }
-//     });
-
 //  //************** */ получаем один документ по API (json) по _id (у всех есть)*****************
 
 // app.get('/api/:_id', async (req, res) => {
@@ -145,23 +133,6 @@ async function run() {
     });
 
 
-
-
-//         app.post('/api', urlencodedParser, async (req, res) => {
-//       try {
-//          const name = req.body.name;
-//          const age = req.body.age;
-//          const newUser = { name, age };
-//         // Получаем все документы из коллекции
-//         result = await collection.insertOne(newUser);
-//         // res.status(200).json(us);
-//         res.json(newUser);
-
-//       } catch (err) {
-//         res.status(500).json({ message: "Ошибка", error: err });
-//       }
-//     });
-
 // //************** */ Редактируем конкретный документ по API ***************** ЕСТЬ КОСЯК!!! (используется только id без _id)
         app.post('/api/edit',urlencodedParser, async (req, res) => {
       try {
@@ -197,58 +168,31 @@ async function run() {
 
 
 
-//  // ДОРАБОТАТЬ!     
-// // app.post('/api/edit', urlencodedParser, async (req, res) => {
-// //       try {
-         
-// //          const id = req.body._id;
-// //          const name = req.body.name;
-// //          const age = req.body.age;
-// //          const newUser = { id, name, age };
-// //         // Получаем все документы из коллекции
-// //         result = await collection.findOneAndUpdate({_id: id}, { $set: {name: name, age: age}}, { returnDocument: "after" });;
-// //         // res.status(200).json(us);
-// //         res.json(newUser);
-
-// //       } catch (err) {
-// //         res.status(500).json({ message: "Ошибка", error: err });
-// //       }
-// //     });
-
-
-// app.post('/api/edit', urlencodedParser, async (req, res) => {
-//   try {
-//     const id = req.body._id;
-//     const name = req.body.name;
-//     const age = req.body.age;
-
-//     if (!ObjectId.isValid(id)) {
-//       return res.status(400).json({ message: "Неверный формат _id" });
-//     }
-
-//     const filter = { _id: new ObjectId(id) };
-//     const updateDoc = {
-//       $set: {
-//         name: name,
-//         age: age
-//       }
-//     };
-
-//     const options = { returnDocument: "after" }; // Возвращает обновленный документ
-
-//     const result = await collection.findOneAndUpdate(filter, updateDoc, options);
-
-//     // if (!result.value) {
-//     //   return res.status(404).json({ message: "Документ не найден" });
-//     // }
-
-//     res.status(200).json(updateDoc);
-
-//   } catch (err) {
-//     res.status(500).json({ message: "Ошибка при обновлении", error: err.message });
-//   }
-// });
 // //************** */ Удаляем документ по API *****************
+
+        app.post('/delete/:_id',urlencodedParser, async (req, res) => {
+      try {
+        // Получаем      
+         const id = req.body._id;
+         
+         if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Неверный формат _id" });
+    }
+
+    
+    const result = await Colltab.findByIdAndDelete(id)
+    console.log(result);
+         
+        res.status(200).json(result);
+                
+
+      } catch (err) {
+        res.status(500).json({ message: "Ошибка при получении данных", error: err });
+      }
+    });
+
+
+
 //         app.post('/delete/:_id',urlencodedParser, async (req, res) => {
 //       try {
 //           const id = req.params._id;
